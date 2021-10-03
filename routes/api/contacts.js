810925b9d314595ed29a0ec117f84197748c3663
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-10-03 13:48:31 +0300

    creates file with routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,63 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { listContacts, getContactById } = require("../../model/index");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        contacts,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { id } = req.params;
+  getContactById(id)
+    .then((contact) =>
+      res.json({
+        status: "success",
+        code: 200,
+        data: {
+          contact,
+        },
+      })
+    )
+    .catch(console.error);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+  } catch {}
+  const { name, email, phone } = req.body;
+  //const newContact = { id: crypto.randomUUID(), name, email, phone };
+  //contacts.push(newContact);
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    // data: {
+    //   newContact,
+    // },
+  });
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.patch("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
