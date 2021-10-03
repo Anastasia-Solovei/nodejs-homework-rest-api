988773712964106c137101f8917598891d6dee68
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-10-03 13:49:44 +0300

    creates file with methods for working with data

--- model/index.js
+++ model/index.js
@@ -1,20 +1,47 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const crypto = require("crypto");
+const DB = require("./db");
+const db = require("./contacts.json");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  return await db.read();
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
 
-const addContact = async (body) => {}
+    const [result] = contacts.filter((contact) => contact.id === contactId);
+    return result;
+  } catch (error) {
+    return error.message;
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+// const removeContact = async (contactId) => {};
+
+const addContact = async (body) => {
+  const contacts = await db.read();
+  const newContact = {};
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await db.read();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    contacts[index] = { ...contacts[index], ...body };
+    await db.write(contacts);
+    return contacts[index];
+  }
+  return null;
+};
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
-  addContact,
+  //   removeContact,
+  //   addContact,
   updateContact,
-}
+};
