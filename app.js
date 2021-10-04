10ced8626e6b985e03c29491fdd2617078404c4f
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-10-04 19:14:02 +0300

    deletes unuseful comments in app file

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-// const createError = require("http-errors");
 const express = require("express");
-// const path = require("path");
-// const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./routes/api/contacts");
+const contactsRouter = require("./routes/api/contacts_router");
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
-
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/api/contacts", contactsRouter);
 
 app.use((_req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ status: "error", code: 404, message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
