a741ecd1d2e7a4efc97c57f02497c07eac8b6ae3
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-10-05 21:38:06 +0300

    adds joi validation to the contacts router

--- routes/api/contacts_router.js
+++ routes/api/contacts_router.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const contactsController = require("../../controllers/contacts_controller");
+const { validateContact } = require("./validation");
 
 router.get("/", contactsController.listContacts);
 
 router.get("/:contactId", contactsController.getContactById);
 
-router.post("/", contactsController.addContact);
+router.post("/", validateContact, contactsController.addContact);
 
 router.delete("/:contactId", contactsController.removeContact);
 
-router.put("/:contactId", contactsController.updateContact);
+router.put("/:contactId", validateContact, contactsController.updateContact);
 
 module.exports = router;
