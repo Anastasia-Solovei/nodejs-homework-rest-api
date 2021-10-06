827aa25516ef8abc8af6d99913606656921c24cb
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-10-06 20:21:09 +0300

    changes path of contactsrouter

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./routes/api/contacts_router");
+const contactsRouter = require("./src/routes/api/contacts_router");
 
 const app = express();
 
