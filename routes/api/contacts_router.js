23c32df0cfbb3849a21f7d6dd59448cb84a31832
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-10-28 20:34:12 +0300

    changes names of the controllers in contacts router file, adds method patch

--- routes/api/contacts_router.js
+++ routes/api/contacts_router.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const contactsController = require("../../controllers/contacts_controller");
-const { validateContact, validateId } = require("./validation");
+const {
+  validateContact,
+  validateId,
+  validateStatusContact,
+} = require("./validation");
 
-router.get("/", contactsController.listContacts);
+router.get("/", contactsController.getContacts);
 
-router.get("/:contactId", validateId, contactsController.getContactById);
+router.get("/:contactId", validateId, contactsController.getById);
 
-router.post("/", validateContact, contactsController.addContact);
+router.post("/", validateContact, contactsController.create);
 
-router.delete("/:contactId", validateId, contactsController.removeContact);
+router.delete("/:contactId", validateId, contactsController.remove);
 
 router.put(
   "/:contactId",
   validateId,
   validateContact,
-  contactsController.updateContact
+  contactsController.update
+);
+
+router.patch(
+  "/:contactId/favorite",
+  validateId,
+  validateStatusContact,
+  contactsController.updateStatus
 );
 
 module.exports = router;
