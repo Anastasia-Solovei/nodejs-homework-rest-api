d913fd6d096517facd3bac556e9980b21193cc23
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-10-31 10:04:10 +0200

    changes name of file and adds constant HttpCode

--- config/constant.js
+++
@@ -1,6 +0,0 @@
-const ValidContactName = {
-  MIN_LENGTH: 1,
-  MAX_LENGTH: 20,
-};
-
-module.exports = { ValidContactName };
---
+++ config/constants.js
@@ -0,0 +1,19 @@
+const ValidContactName = {
+  MIN_LENGTH: 1,
+  MAX_LENGTH: 20,
+};
+
+const HttpCode = {
+  OK: 200,
+  CREATED: 201,
+  ACCEPTED: 202,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+module.exports = { ValidContactName, HttpCode };
