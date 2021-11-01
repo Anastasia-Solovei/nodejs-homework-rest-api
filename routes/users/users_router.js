d1f9eb8f1498b409063548e61cbeebef0c21fa1b
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-11-01 22:34:49 +0200

    adds user validation, subscription status validation and loginLimiter middleware

--- routes/users/users_router.js
+++ routes/users/users_router.js
@@ -2,17 +2,22 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../../controllers/users_controller");
 const guard = require("../../helpers/guard");
-const { validateUser } = require("./validation");
+const loginLimiter = require("../../helpers/rate-limit-login");
+const { validateUser, validateSubscriptionStatus } = require("./validation");
 
 router.post("/signup", validateUser, usersController.signup);
 
-router.post("/login", validateUser, usersController.login);
+router.post("/login", loginLimiter, validateUser, usersController.login);
 
 router.post("/logout", guard, usersController.logout);
 
 router.get("/current", guard, usersController.current);
-// GET / users / current;
 
-// Authorization: "Bearer {{token}}";
+router.patch(
+  "/",
+  guard,
+  validateSubscriptionStatus,
+  usersController.updateSubscription
+);
 
 module.exports = router;
