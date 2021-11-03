508e64587e145529cd2637678d26a6ac0bb832bc
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-11-03 23:25:05 +0200

    creates directories for temporary uploading and storage of avatars

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,17 @@
 const db = require("../config/db");
 const app = require("../app");
+require("dotenv").config();
+const TMP_DIR = process.env.TMP_DIR;
+const PUBLIC_AVATARS = process.env.PUBLIC_AVATARS;
+
+const mkdirp = require("mkdirp");
 
 const PORT = process.env.PORT || 3000;
 
 db.then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    await mkdirp(TMP_DIR);
+    await mkdirp(PUBLIC_AVATARS);
     console.log(`Server running. Use our API on port: ${PORT}`);
   });
 }).catch((err) => {
