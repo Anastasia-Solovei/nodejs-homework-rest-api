90f395f60fef1205d612e2071f0fac5ad84643b5
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-11-06 17:42:51 +0200

    creating endpoints for email verification and resending email to user with verification link, also adds custom error handling

--- routes/users/users_router.js
+++ routes/users/users_router.js
@@ -4,28 +4,40 @@ const usersController = require("../../controllers/users_controller");
 const guard = require("../../helpers/guard");
 const loginLimiter = require("../../helpers/rate-limit-login");
 const uploads = require("../../helpers/uploads");
-const { validateUser, validateSubscriptionStatus } = require("./validation");
+const {
+  validateUser,
+  validateSubscriptionStatus,
+  validateEmail,
+} = require("./validation");
+const wrapError = require("../../helpers/error_handler");
 
-router.post("/signup", validateUser, usersController.signup);
-
-router.post("/login", loginLimiter, validateUser, usersController.login);
-
-router.post("/logout", guard, usersController.logout);
-
-router.get("/current", guard, usersController.current);
-
+router.post("/signup", validateUser, wrapError(usersController.signup));
+router.post(
+  "/login",
+  loginLimiter,
+  validateUser,
+  wrapError(usersController.login)
+);
+router.post("/logout", guard, wrapError(usersController.logout));
+router.get("/current", guard, wrapError(usersController.current));
 router.patch(
   "/",
   guard,
   validateSubscriptionStatus,
-  usersController.updateSubscription
+  wrapError(usersController.updateSubscription)
 );
-
 router.patch(
   "/avatars",
   guard,
   uploads.single("avatar"),
-  usersController.uploadAvatar
+  wrapError(usersController.uploadAvatar)
+);
+
+router.get("/verify/:verificationToken", wrapError(usersController.verifyUser));
+router.post(
+  "/verify",
+  validateEmail,
+  wrapError(usersController.repeatEmailForVerifyUser)
 );
 
 module.exports = router;
